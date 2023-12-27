ff6670669aa0ece85d74835e285107ad68030461
qpackman <khyhlee@naver.com>
2023-12-27 19:22:23 +0900

    hbj

---
+++ src/Chap_01/_02_datatype.java
@@ -0,0 +1,14 @@
+package Chap_01;
+
+public class _02_datatype {
+    public static void main(String[] args) {
+        System.out.println("_02_datatype.main");
+        System.out.println("안녕하세요?");
+        System.out.println(12);
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(23+20);
+    }
+
+
+}
---
+++ src/Chap_01/_03_variables.java
@@ -0,0 +1,11 @@
+package Chap_01;
+
+public class _03_variables {
+    public static void main(String[] args) {
+        String name;
+        name="이규현";
+
+        System.out.println(name +"공부 시작");
+        System.out.println(name +"공부 종료");
+    }
+}
